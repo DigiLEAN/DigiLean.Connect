727f30854969eb98d8f881d32580e85a62a601a4
Atle Magnussen <atlemagnussen@gmail.com>
2024-08-28 09:04:41 +0200

    upd

--- dotnet/DigiLean.Api.Model/V1/Incident/IncidentPagedValues.cs
+++ dotnet/DigiLean.Api.Model/V1/Incident/IncidentPagedValues.cs
@@ -1,12 +1,11 @@
 ﻿using DigiLean.Api.Model.Common;
-using DigiLean.Api.Model.V1.Incident;
 
 namespace DigiLean.Api.Model.V1.Incident
 {
     /// <summary>
     /// For API v1
     /// </summary>
-    public class IncidentPagedValues : PagedValues<IncidentInfo>
+    public class IncidentPagedValues : PagedValues<Incident>
     {
     }
 }
